789e6afd8af30dcbffa9e6aa8aee6b11d1ad021c
Sam Morley-Short <sam.morleyshort@gmail.com>
2020-10-14 21:41:46 -0700

    [setup.py] Refactored into package structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pg_alert',
+    version='0.0.1',
+    # url='https://github.com/mypackage.git',
+    author='Sam Morley-Short',
+    author_email='sam.morleyshort@gmail.com',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'selenium>=3.141.0,<4.0.0',
+        'python-dateutil>=2.8.1,<3.0.0',
+    ],
+)
