c96cb87086b78af62014bbe9eeb4065711a911be
Sam Morley-Short <sam.morleyshort@gmail.com>
2021-03-04 23:00:28 -0800

    [main] Now returning per-user attendability data

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import logging
 from pprint import pprint
 from pg_alert.scrape import get_all_bookable_slots
-from pg_alert.availability import check_availability
+from pg_alert.availability import find_attendable_slots
 from pg_alert.sheets import read_signup_sheet
 from pg_alert.io import save_to_json
+from pg_alert.errors import UnsupportedGym
 
 
 def run():
-    bookable_slots = get_all_bookable_slots()
-    print(f"Bookable slots:")
-    pprint(bookable_slots)
-
+    # Read the sign-up data and then dump it to a JSON
     signup_data = read_signup_sheet()
+    save_to_json(signup_data, 'signups.json')
     print("Signup data:")
     pprint(signup_data)
-    save_to_json(signup_data, 'signups.json')
+
+    # Compile list of attendable slots
+    all_attendable_slots = {}
+    for gym_name, gym_signups in signup_data.items():
+        try:
+            bookable_slots = get_all_bookable_slots(gym_name)
+        except UnsupportedGym:
+            print(f"Skipping unknown gym {gym_name}")
+            continue
+        print(f"Bookable slots for {gym_name}:")
+        pprint(bookable_slots)
+
+        for gym_signup in gym_signups:
+            name = gym_signup['name']
+            email = gym_signup['email']
+            availability = gym_signup['availability']
+            attendable_slots = find_attendable_slots(availability, bookable_slots)
+            if attendable_slots:
+                all_attendable_slots.setdefault((name, email), {})
+                all_attendable_slots[(name, email)][gym_name] = attendable_slots
+
+            print(f"User {name} can attend slots:")
+            print(attendable_slots)
+
+    print(all_attendable_slots)
 
     # example_data = {
     #     'Mon': ['08:00-14:00', '16:00-23:59'],
